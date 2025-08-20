791a757734b57df38c69c8d597235858f8f3ff9b
langelxobl <langelxobl@gmail.com>
2025-08-20 00:33:47 -0500

    feat: add action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: 'http://lawyercontreras.com/',
+
   i18n: {
     defaultLocale: 'es',
     locales: ['en', 'es'],
@@ -13,11 +16,15 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
+
   redirects: {
     '/': '/es/',
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [sitemap()],
+  adapter: cloudflare(),
 });
